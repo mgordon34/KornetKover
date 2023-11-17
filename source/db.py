a0dc37074e13ac797aa6409b6a17655e7863b5bf
mgordon34 <coltfanman@gmail.com>
2023-11-17 14:53:49 -0700

    created DB class

---
+++ source/db.py
@@ -0,0 +1,34 @@
+import logging
+
+import psycopg2
+
+logger = logging.getLogger(__name__)
+
+class DB(object):
+    def __init__(self):
+        self.conn = self.create_connection()
+
+    def create_connection(self):
+        conn = None
+        try:
+            conn = psycopg2.connect(
+                database="postgres",
+                host="localhost",
+                user="postgres",
+                password="password",
+                port="5432"
+            )
+            logger.info("DB connection successful")
+        except Error as e:
+            logger.error(e)
+        return conn
+
+    def get_games(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * from games;")
+        res = cursor.fetchall()
+        return res
+
+if __name__ == "__main__":
+    db = DB()
+    print(db.get_games())
