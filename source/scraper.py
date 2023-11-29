30ecb304e4ec599cbe892afdda8a35484d8bba70
Matt Gordon <coltfanman@gmail.com>
2023-11-29 08:00:07 -0700

    more game scraping

--- source/scraper.py
+++ source/scraper.py
@@ -29,14 +29,19 @@ class Scraper(object):
     def scrape_games(cls, start_date, end_date):
         url = "https://www.basketball-reference.com/boxscores/?month=%s&day=%s&year=%s"
         cur_date = start_date
-        while (cur_date < end_date):
+        while cur_date <= end_date:
             print(f"scraping for {cur_date}")
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, "html.parser")
+            games = soup.find_all("div", "game_summary")
+            for game in games:
+                print(game)
 
             cur_date += timedelta(days=1)
 
 
 if __name__ == "__main__":
     # db = DB()
-    start_date = datetime.strptime('2023-11-16', '%Y-%m-%d').date()
-    end_date = datetime.strptime('2023-11-23', '%Y-%m-%d').date()
+    start_date = datetime.strptime('2023-04-21', '%Y-%m-%d').date()
+    end_date = datetime.strptime('2023-04-21', '%Y-%m-%d').date()
     Scraper.scrape_games(start_date, end_date)
