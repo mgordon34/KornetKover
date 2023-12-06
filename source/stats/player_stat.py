5eaf76be34d36afce2690a32122ca4a2021faf35
mgordon34 <coltfanman@gmail.com>
2023-12-06 13:32:02 -0700

    finished player_avgs fcn

--- source/stats/player_stat.py
+++ source/stats/player_stat.py
@@ -1,7 +1,17 @@
 
 class PlayerStat(object):
-    def __init__(self, num_games: int, points: float, rebounds: float, assists: float) -> None:
+    def __init__(
+        self,
+        frame: int,
+        num_games: int,
+        minutes: float,
+        points: float,
+        rebounds: float,
+        assists: float
+    ) -> None:
+        self.frame = frame
         self.num_games = num_games
+        self.minutes = minutes
         self.points = points
         self.rebounds = rebounds
         self.assists = assists
--- source/stats/player_stat_service.py
+++ source/stats/player_stat_service.py
@@ -13,15 +13,16 @@ class PlayerStatService(object):
         player_index: str,
         start_date: str,
         end_date: str,
+        frame: int,
     ) -> dict:
-        sql = """SELECT COUNT(*), AVG(pg.points), AVG(pg.assists), AVG(pg.rebounds) FROM player_games pg
+        sql = """SELECT COUNT(*), AVG(pg.minutes), AVG(pg.points), AVG(pg.assists), AVG(pg.rebounds) FROM player_games pg
                  LEFT join games gg ON gg.id=pg.game
                  WHERE pg.player_index='{0}' AND gg.date>'{1}' AND gg.date<'{2}'"""
         
         res = self.db.execute_query(sql.format(player_index, start_date, end_date))
-        stats = PlayerStat(res[0], float(res[1]), float(res[2]), float(res[3]))
+        stats = PlayerStat(frame, res[0], float(res[1]), float(res[2]), float(res[3]), float(res[4]))
 
-        print(stats.points)
+        return stats
 
 
 if __name__ == "__main__":
@@ -31,6 +32,7 @@ if __name__ == "__main__":
     start_date = "2022-10-10"
     end_date = "2023-04-10"
     player_index = "tatumja01"
-    pss.calc_player_avgs(player_index, start_date, end_date)
+    frame = 1
+    pss.calc_player_avgs(player_index, start_date, end_date, frame)
 
     db.close()
