9c35e1449687e3eab1808d3c751b2f77f0d9bc46
Matt Gordon <coltfanman@gmail.com>
2023-12-19 16:27:56 -0500

    added final player analysis

--- kornetkover/analysis/matchup_analysis_service.py
+++ kornetkover/analysis/matchup_analysis_service.py
@@ -25,9 +25,9 @@ class MatchupAnalysisService(object):
 
     def analyze_player_matchups(self, team_one, team_two, p_threshold=25, date=datetime.now().date()):
         for player in team_one["starting"]:
-            player_analysis = PlayerAnalysis(player, date)
             player_stats = self.pss.calc_player_avgs_by_year(player, str_to_date(self.start_date), str_to_date(self.end_date))
             current_stats = player_stats[get_nba_year_from_date(str_to_date(self.end_date))]
+            player_analysis = PlayerAnalysis(player, date, current_stats)
             print("----------analyzing matchups for {}: MIN[{}], PTS[{}], REB[{}], AST[{}]----------".format(
                 player,
                 round(current_stats.minutes, 2),
@@ -69,6 +69,19 @@ class MatchupAnalysisService(object):
                     predicted_stat = self.get_predicted_value(current_stat_value, stat_pchange)
                     print(f"[{pip_factor.num_games}]{player} matchup with {matchup} leads to {stat_pchange} change in {stat}: {predicted_stat}")
 
+            player_analysis.prediction = self.predict_stats(player_analysis.pip_factors, current_stats)
+            outliers = self.find_outlying_stats(player_analysis.prediction, current_stats)
+            if outliers and len(player_analysis.pip_factors) > 5:
+                [print(f"Notable Outlier {outlier}: {outliers[outlier]}") for outlier in outliers]
+            print("Predicted stats: MIN[{}], PTS[{}], REB[{}], AST[{}]".format(
+                player_analysis.prediction.minutes,
+                player_analysis.prediction.points,
+                player_analysis.prediction.rebounds,
+                player_analysis.prediction.assists,
+            ))
+
+            
+
     def find_notable_stats(self, pip_factor: PipFactor, p_threshold: int):
         stats = ["minutes", "points", "rebounds", "assists"]
 
@@ -84,5 +97,43 @@ class MatchupAnalysisService(object):
         predicted_value = current_value + (current_value * pchange/100)
         return round(predicted_value, 2)
 
-    def predict_stats(player_analysis):
-        return
+    def predict_stats(self, pip_factors: list[PipFactor], player_avgs: PlayerPer) -> PlayerStat:
+        predicted_stats = {
+            "minutes": 0,
+            "points": 0,
+            "rebounds": 0,
+            "assists": 0,
+        }
+        count = 0
+        for pip_factor in pip_factors:
+            for stat in predicted_stats:
+                yearly_stat = getattr(player_avgs, stat)
+                stat_pchange = getattr(pip_factor, stat)
+                if stat == "minutes":
+                    predicted_stat = self.get_predicted_value(yearly_stat, stat_pchange)
+                else:
+                    predicted_stat = self.get_predicted_value(yearly_stat, stat_pchange) * player_avgs.minutes
+                
+                predicted_stats[stat] = (predicted_stats[stat] * count + predicted_stat * pip_factor.num_games) / float(count + pip_factor.num_games)
+            count = count + pip_factor.num_games
+        
+        return PlayerStat(
+            round(predicted_stats["minutes"], 2),
+            round(predicted_stats["points"], 2),
+            round(predicted_stats["rebounds"], 2),
+            round(predicted_stats["assists"], 2),
+        )
+    
+    def find_outlying_stats(self, predicted_stats: PlayerStat, yearly_stats: PlayerPer):
+        stats = ["minutes", "points", "rebounds", "assists"]
+        outliers = {}
+        for stat in stats:
+            predicted_stat = getattr(predicted_stats, stat)
+            yearly_avg_stat = getattr(yearly_stats, stat) * yearly_stats.minutes if stat != "minutes" else getattr(yearly_stats, stat)
+
+            stat_diff = predicted_stat - yearly_avg_stat
+            stat_pchange = (stat_diff / yearly_avg_stat) * 100
+            if abs(stat_diff) > 1 and abs(stat_pchange) > 10:
+                outliers[stat] = predicted_stat
+        
+        return outliers
--- kornetkover/analysis/player_analysis.py
+++ kornetkover/analysis/player_analysis.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
+from kornetkover.stats.player_per import PlayerPer
+
 class PlayerAnalysis(object):
-    def __init__(self, player_index: str, date: datetime.date) -> None:
+    def __init__(self, player_index: str, date: datetime.date, yearly_avg_per: PlayerPer) -> None:
         self.player_index = player_index
         self.date = date
+        self.yearly_avg_per = yearly_avg_per
         self.pip_factors = []
         self.notable_stats = []
         self.prediction = None
