09904bf1b46c532067003b600be6d3357fe2f9d2
mgordon34 <coltfanman@gmail.com>
2024-01-04 16:14:08 -0700

    added time on team function

--- kornetkover/players/player_service.py
+++ kornetkover/players/player_service.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from kornetkover.tools.db import DB
 from kornetkover.players.player import Player
 
@@ -27,12 +29,25 @@ class PlayerService(object):
         player = res[0]
         return Player(player[0], player[1])
 
+    def find_date_on_active_team(self, player_index: str, team: str) -> datetime.date:
+        sql = """SELECT gg.date
+                 FROM player_games pg
+                 LEFT join games gg ON gg.id=pg.game
+                 WHERE pg.player_index='{0}' AND pg.team_index!='{1}'
+                 ORDER BY gg.date DESC"""
+
+        res = self.db.execute_query(sql.format(player_index, team))
+        if not res or not res[0]:
+            return None
+
+        return res[0][0] + timedelta(days=1)
+
 
 if __name__ == "__main__":
     db = DB()
     ps = PlayerService(db)
     db.initialize_tables()
 
-    index = "jaime jaquez"
-    player = ps.name_to_player(index)
-    print(player.__dict__)
+    index = "whitede01"
+    date = ps.find_date_on_active_team(index, 'BOS')
+    print(date)
