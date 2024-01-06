e46566e384f415eaadca357ca608f1a33cb7ca7b
mgordon34 <coltfanman@gmail.com>
2024-01-06 11:52:31 -0700

    formatting

--- kornetkover/odds/odds_service.py
+++ kornetkover/odds/odds_service.py
@@ -12,7 +12,12 @@ class OddsService(object):
     def __init__(self, db: DB):
         self.db = db
 
-    def to_player_odds(self, player_index: str, date: datetime.date, prop_lines: List[PropLine]):
+    def to_player_odds(
+        self,
+        player_index: str,
+        date: datetime.date,
+        prop_lines: List[PropLine]
+    ):
         player_odds = []
         for prop_line in prop_lines:
             player_odds.append((player_index, date.strftime("%Y-%m-%d"), prop_line.stat, prop_line.line, prop_line.over_odds, prop_line.under_odds))
@@ -31,18 +36,18 @@ class OddsService(object):
         sql = """SELECT player_index, date, stat, line, over_odds, under_odds
                  FROM player_odds
                  WHERE player_index='{0}' AND date='{1}'"""
-        
+
         res = self.db.execute_query(sql.format(player_index, date))
         player_odds = PlayerOdds(player_index, date)
         for line in res:
             player_odds.add_prop_line(PropLine(*line[2:]))
+
         return player_odds
-        
 
     def update_player_odds_for_date(self, date: datetime.date):
         ps = PlayerService(self.db)
         prop_lines = Scraper.get_prop_lines(date)
-        
+
         player_odds = []
         for name, lines in prop_lines.items():
             player = ps.name_to_player(name)
